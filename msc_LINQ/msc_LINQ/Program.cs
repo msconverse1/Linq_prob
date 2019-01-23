8364b1667a209301dd18f737a6d32f36fff2784a
msconverse1 <msconverse@fullsail.edu>
2019-01-23 10:31:31 -0600

    Created Function that returns a copy of a list without duplicates

--- msc_LINQ/msc_LINQ/Program.cs
+++ msc_LINQ/msc_LINQ/Program.cs
@@ -6,24 +6,37 @@ using System.Threading.Tasks;
 
 namespace msc_LINQ
 {
-    static class Program
+     class Program
     {
         static void Main(string[] args)
         {
-
             List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
-            List<string> substringwords = words.FindAll(q => q.Contains("th"));
+            FindSubString(words);
+            Console.ReadLine();
+            Console.Clear();
+            ReturnCopyListMinusDups();
+            Console.ReadLine();
+        }
+       static void FindSubString(List<string> words)
+        {
+            string tag = "th";
+            
+            List<string> substringwords = words.FindAll(q => q.Contains(tag));
             foreach (var item in substringwords)
             {
                 Console.WriteLine(item);
             }
-            Console.ReadLine();
-
         }
-       
+       static void ReturnCopyListMinusDups()
+        {
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            var copyList = names.Distinct();
+            foreach (var item in copyList)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
     }
-    static  class Term
-    {
-        static public string tag = "th";
-    }
+
 }
