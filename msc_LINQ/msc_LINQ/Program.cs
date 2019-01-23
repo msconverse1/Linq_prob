ce6ea35c30cb1025e565f09437396ab509f8ccc5
msconverse1 <msconverse@fullsail.edu>
2019-01-23 10:15:21 -0600

    Created function that returns all words that contains a substring

---
+++ msc_LINQ/msc_LINQ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace msc_LINQ
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            List<string> substringwords = words.FindAll(q => q.Contains("th"));
+            foreach (var item in substringwords)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+        }
+       
+    }
+    static  class Term
+    {
+        static public string tag = "th";
+    }
+}
